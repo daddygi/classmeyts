a07661352bb79631d6d965bb3f0ac46b8ca50167
Ben-Sicat <bensicat00@gmail.com>
2024-04-21 14:39:31 +0800

    ehw

--- src/app/components/header.tsx
+++ src/app/components/header.tsx
@@ -2,8 +2,8 @@ import React from 'react';
 
 const Header = ({ title }: { title: string }) => {
   return (
-    <header className="py-4 px-8"> 
-      <h1 className="text-2xl font-bold">{title}</h1>
+    <header className="p-1000 p-8 bg-red"> 
+      <h1 className="text-4xl font-bold">{title}</h1>
     </header>
   );
 };
